209b877ea3d859986982ca53d459c14b83514a0c
ShuJianglin <dark_trace@sina.com>
2019-09-28 17:24:17 +0800

    Initial commit

---
+++ src/main/java/com/hunau/myspringboot/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.hunau.myspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("hello")
+public class IndexController {
+    @RequestMapping("123")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("1232")
+    @ResponseBody
+    public String index2() {
+        return "index";
+    }
+
+}
